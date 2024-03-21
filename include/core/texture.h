98ba3aa7f29e8ef2c40328b7089a1f86edd82600
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-03-21 14:31:01 -0300

    Let there be light

---
+++ include/core/texture.h
@@ -0,0 +1,104 @@
+
+#ifndef OPENGLBOOK_TEXTURE_H
+#define OPENGLBOOK_TEXTURE_H
+
+#include <glad/glad.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image/stb_image.h>
+
+
+enum WrapParams {
+    Repeat = GL_REPEAT,
+    MirrorRepeat = GL_MIRRORED_REPEAT,
+    ClampEdge = GL_CLAMP_TO_EDGE,
+    ClampBorder = GL_CLAMP_TO_BORDER
+};
+
+enum MagFilterParams {
+    Nearest = GL_NEAREST,
+    Linear = GL_LINEAR,
+};
+
+enum MinFilterParams {
+    NearestMipmapNearest = GL_NEAREST_MIPMAP_NEAREST,
+    LinearMipmapNearest = GL_LINEAR_MIPMAP_NEAREST,
+    NearestMipmapLinear = GL_NEAREST_MIPMAP_LINEAR,
+    LinearMipmapLinear = GL_LINEAR_MIPMAP_LINEAR,
+};
+
+class Texture {
+
+public:
+    unsigned int ID;
+    unsigned int textureAddr;
+
+    Texture() {
+        glGenTextures(1, &ID);
+        textureAddr = GL_TEXTURE0;
+        bind();
+    }
+
+    Texture(int texAddrIndex) {
+        glGenTextures(1, &ID);
+        textureAddr = GL_TEXTURE0 + texAddrIndex;
+        bind();
+    }
+
+
+    Texture* setWrapX(WrapParams param) {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, param);
+
+        return this;
+    }
+
+    Texture* setWrapY(WrapParams param) {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, param);
+
+        return this;
+    }
+
+    Texture* setMagFilter(MagFilterParams param) {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, param);
+
+        return this;
+    }
+
+
+    Texture* setMinFilter(MinFilterParams param) {
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, param);
+
+        return this;
+    }
+
+    Texture* loadImage(std::string filename, unsigned int internalFormat, unsigned int format) {
+        stbi_set_flip_vertically_on_load(true);
+
+        int width, height, nrChannels;
+        unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrChannels, 0);
+        if (data) {
+            glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        } else {
+            std::cout << "ERROR::LOADING_TEXTURE\n" << std::endl;
+        }
+        stbi_image_free(data);
+
+        return this;
+    }
+
+    void activate() {
+        glActiveTexture(textureAddr);
+    }
+
+    void bind() {
+        glBindTexture(GL_TEXTURE_2D, ID);
+    }
+
+    void use() {
+        activate();
+        bind();
+    }
+};
+
+#endif //OPENGLBOOK_TEXTURE_H
---
+++ src/main.cpp
@@ -0,0 +1,151 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <core/shader.h>
+#include <core/texture.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void process_input(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main (int argc, char** argv)
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Build and compile our shader program
+    // -----------------------------------------------------------
+
+    Shader shader("assets/shaders/shader.vert", "assets/shaders/shader.frag");
+
+
+    // Set up vertex data and configure vertex attrib
+    // ---------------------------------------------------
+
+
+    float vertices[] = {
+            // positions          // colors           // boxTexture coords
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+            0, 1, 3, // first triangle
+            1, 2, 3  // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // Bind VAO and VBO
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // First: The position of the vertex attrib, Second: The size of the vertex Attrib, Third> Type of data, Fourth: The stride, where the next attrib starts, the gap. Last: The offset, where the data beggins in the buffer.
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // Set the Attribute pointer for the Colors data in the Vertex Array
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof (float ), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof (float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+
+    // Unbind VAO and VBO
+    // The VBO unbind is safe because everything has been saved inside the VAO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+
+
+    // Load faceTexture
+    // -------------------------------------
+    Texture faceTexture(1);
+
+    faceTexture
+        .setWrapX(WrapParams::Repeat)
+        ->setWrapY(WrapParams::Repeat)
+        ->setMinFilter(MinFilterParams::LinearMipmapLinear)
+        ->setMagFilter(MagFilterParams::Linear)
+        ->loadImage("assets/images/awesomeface.png", GL_RGBA, GL_RGBA);
+
+
+    // Load boxTexture
+    // ----------------------------------------------------
+    Texture boxTexture;
+
+    boxTexture
+            .setWrapX(WrapParams::Repeat)
+            ->setWrapY(WrapParams::Repeat)
+            ->setMinFilter(MinFilterParams::LinearMipmapLinear)
+            ->setMagFilter(MagFilterParams::Linear)
+            ->loadImage("assets/images/container.jpg", GL_RGB, GL_RGB);
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        boxTexture.use();
+        faceTexture.use();
+
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Free resources
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // Terminate cleaning all previous allocate GLFW resources
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
